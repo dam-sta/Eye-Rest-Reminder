2dbfaf9ba9cb84a0927d04fc49dbed6d2274bf95
Damian <staszekdamian2004@gmail.com>
2024-07-28 19:02:37 +0200

    finished project

---
+++ eyeRestReminder/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace eyeRestReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DispatcherTimer forHowLongToShow = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer whenToShow = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromHours(1)
+            };
+            whenToShow.Tick += WhenToShowTicker;
+            whenToShow.Start();
+
+            forHowLongToShow.Interval = TimeSpan.FromSeconds(5);
+            forHowLongToShow.Tick += ForHowLongToShowTicker;
+
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+
+        }
+
+        //make main window click-through
+        public static class WindowsServices
+        {
+            const int WS_EX_TRANSPARENT = 0x00000020;
+            const int GWL_EXSTYLE = (-20);
+
+            [DllImport("user32.dll")]
+            static extern int GetWindowLong(IntPtr hwnd, int index);
+
+            [DllImport("user32.dll")]
+            static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+            public static void SetWindowExTransparent(IntPtr hwnd)
+            {
+                var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+                SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            }
+        }
+
+
+
+        //disable focus
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            //Set the window style to noactivate.
+            var helper = new WindowInteropHelper(this);
+            SetWindowLong(helper.Handle, GWL_EXSTYLE,
+                GetWindowLong(helper.Handle, GWL_EXSTYLE) | WS_EX_NOACTIVATE);
+
+            //click-through
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+
+        private void WhenToShowTicker(object sender, EventArgs e)
+        {
+            mainWindow.Visibility = Visibility.Visible;
+            forHowLongToShow.Start();
+        }
+
+        private void ForHowLongToShowTicker(object sender, EventArgs e)
+        {
+            mainWindow.Visibility = Visibility.Hidden;
+            forHowLongToShow.Stop();
+
+        }
+
+    }
+}
